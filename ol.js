3e988eef0c657e04b40408eee892fd8eca33d280
Charles Horn <charles.horn@gmail.com>
2017-11-03 11:01:12 +1300

    create admin sync button for editions

--- ol.js
+++ ol.js
@@ -1,8 +1,5 @@
 // Looks for, and highlights, editions without works on Open Library search results pages
 
-results = document.getElementById("siteSearch")
-items = results.getElementsByClassName("searchResultItem")
-
 function associate_work(work_id) {
    editions = []
    $("input:checkbox[name=orphan]:checked").each(function(){
@@ -14,27 +11,48 @@ function associate_work(work_id) {
    $( "<div>" + txt + "</div>" ).dialog();
 }
 
-var script = document.createElement('script')
-script.type = 'text/javascript'
-script.innerHTML = associate_work
-document.getElementsByTagName('head')[0].appendChild(script)
+results = document.getElementById("siteSearch")
+if (results) {
+    items = results.getElementsByClassName("searchResultItem")
 
-for (let item of items) {
-   href = item.getElementsByTagName("a")[0].getAttribute("href")
-   if (/^\/works\/OL[0-9]+M/.test(href)) { 
-       id = /OL[0-9]+M/.exec(href)
-       item.style.backgroundColor = "darksalmon";
-       item.getElementsByClassName("booktitle")[0].innerHTML += "ORPHANED EDITION: " + id
-       checkbox = document.createElement("INPUT")
-       checkbox.setAttribute('type', 'checkbox')
-       checkbox.setAttribute('name', 'orphan')
-       checkbox.setAttribute('value', id)
-       item.insertBefore(checkbox, item.firstChild)
-   } else { // it is a work
-       work = /OL[0-9]+W/.exec(href)
+    var script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.innerHTML = associate_work
+    document.getElementsByTagName('head')[0].appendChild(script)
+
+    for (let item of items) {
+       href = item.getElementsByTagName("a")[0].getAttribute("href")
+       if (/^\/works\/OL[0-9]+M/.test(href)) {
+           id = /OL[0-9]+M/.exec(href)
+           item.style.backgroundColor = "darksalmon";
+           item.getElementsByClassName("booktitle")[0].innerHTML += "ORPHANED EDITION: " + id
+           checkbox = document.createElement("INPUT")
+           checkbox.setAttribute('type', 'checkbox')
+           checkbox.setAttribute('name', 'orphan')
+           checkbox.setAttribute('value', id)
+           item.insertBefore(checkbox, item.firstChild)
+       } else { // it is a work
+           work = /OL[0-9]+W/.exec(href)
+           btn = document.createElement("BUTTON")
+           btn.innerHTML = "associate"
+           btn.setAttribute('onclick', 'associate_work("' + work + '")')
+           item.insertBefore(btn, item.firstChild)
+       }
+    }
+}
+
+function get_olid() {
+    return(window.location.pathname.match(/OL[0-9]+M/)[0])
+}
+
+// Add admin sync button on Edition pages for editions with OCAIDs
+titles = document.querySelectorAll('span.title')
+for (let title of titles) {
+    if (title.innerHTML.includes("Internet Archive")) {
+       console.log("FOUND IT!")
        btn = document.createElement("BUTTON")
-       btn.innerHTML = "associate"
-       btn.setAttribute('onclick', 'associate_work("' + work + '")')
-       item.insertBefore(btn, item.firstChild)
-   }
+       btn.innerHTML = "sync"
+       btn.setAttribute('onclick', "window.open('https://openlibrary.org/admin/sync?edition_id=" + get_olid() + "');")
+       title.insertBefore(btn, title.firstChild)
+    }
 }
