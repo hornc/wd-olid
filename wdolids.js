e7f7b95b90978a8b3f8dbc21b43e07f1418c7882
Charles Horn <charles.horn@gmail.com>
2020-09-19 17:29:30 +1200

    SPARQL for more than 4 olids

--- wdolids.js
+++ wdolids.js
@@ -2,7 +2,7 @@ var body = document.getElementById('bodyContent');
 
 var ol = document.getElementById('P648');
 
-var query_olids = "<a href=\"https://query.wikidata.org/#%23People%20with%20more%20than%20one%20OLID%0ASELECT%20%3Fitem%20%3FitemLabel%20%3Fcount%0AWHERE%0A%7B%0A%20%20%7B%0A%20%20%20%20SELECT%20%3Fitem%20%28COUNT%28DISTINCT%20%3Folid%29%20AS%20%3Fcount%29%20WHERE%20%7B%0A%20%20%20%20%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%20.%0A%20%20%20%20%20%20%3Fitem%20wdt%3AP648%20%3Folid%20.%0A%0A%20%20%20%20%7D%0A%20%20%20%20GROUP%20BY%20%3Fitem%0A%20%20%7D%0A%20%20FILTER%20%28%20%3Fcount%20%3E%201%20%29%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20.%20%7D%0A%7D%0AORDER%20BY%20DESC%28%3Fcount%29%20%3FitemL\">QUERY DUPED OLIDS</a>"
+var query_olids = "<a href=\"https://query.wikidata.org/#SELECT%20%3Fitem%20%28GROUP_CONCAT%28DISTINCT%20%3Folid%3B%20separator%3D%22%2C%20%22%29%20as%20%3Folids%29%20%28count%28%3Folid%29%20as%20%3Fnum%29%0AWHERE%20%7B%0A%20%20%3Fitem%20wdt%3AP648%20%3Folid%20.%20%20%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%20.%0A%20%20%7D%20%0AGROUP%20BY%20%3Fitem%0AHAVING%28count%28%3Folid%29%20%3E%204%29%0AORDER%20BY%20DESC%28%3Fnum%29%20STRLEN%28STR%28%3Fitem%29%29%20STR%28%3Fitem%29\">QUERY DUPED OLIDS</a>"
 
 function getProp(prop_id) {
   if (document.getElementById(prop_id)) {
